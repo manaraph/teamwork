9aedd497df42e30b8d3181b0f18d53f1fe952f52
manaraph <manaraph225@gmail.com>
2019-11-11 00:14:12 +0100

    Lint fixes

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,11 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
-// const dotenv = require("dotenv");
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
 
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('connect', () => {
@@ -15,63 +14,66 @@ pool.on('connect', () => {
 
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM employees ORDER BY id ASC', (error, results) => {
-      if(error){
-          throw error
-      }
-      response.status(200).json(results.rows)
-  })
-}
-const createUser = (request, response) => { 
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+const createUser = (request, response) => {
   console.log('Creating user');
   console.log(request.query);
-  
-     
-  const{email, firstname, lastname, password, role} = request.query;
+
+
+  const {
+    email, firstname, lastname, password, role,
+  } = request.query;
 
   pool.query('INSERT INTO employees (email, firstname, lastname, password, role) VALUES ($1, $2, $3, $4, $5)', [email, firstname, lastname, password, role], (error, results) => {
-      if(error){
-          throw error;
-      }
-      console.log(results);
-      
-      response.status(201).send(`User added with ID: ${results}`);
-  })
+    if (error) {
+      throw error;
+    }
+    console.log(results);
+
+    response.status(201).send(`User added with ID: ${results}`);
+  });
   console.log('User created');
-}
+};
 
 
 const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const{email, firstname, lastname, password, role} = request.query;
-  
+  const id = parseInt(request.params.id);
+  const {
+    email, firstname, lastname, password, role,
+  } = request.query;
+
 
   pool.query(
-      'UPDATE employees SET email = $1, firstname = $2, lastname = $3, password = $4, role = $5 WHERE id = $6',
-      [email, firstname, lastname, password, role, id],
-      (error, results) => {
-          if(error){
-              throw error
-          }
-          response.status(200).send(`User modified with ID: ${id}`)
+    'UPDATE employees SET email = $1, firstname = $2, lastname = $3, password = $4, role = $5 WHERE id = $6',
+    [email, firstname, lastname, password, role, id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-  )
-}
+      response.status(200).send(`User modified with ID: ${id}`);
+    },
+  );
+};
 
 const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
+  const id = parseInt(request.params.id);
 
   pool.query('DELETE FROM employees WHERE id = $1', [id], (error, results) => {
-      
-      if(error){
-          throw error
-      }
-      response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
 
 module.exports = {
   getUsers,
   createUser,
-  updateUser, 
-  deleteUser
-}
+  updateUser,
+  deleteUser,
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import express from "express";
-import { getUsers, createUser, updateUser, deleteUser } from "./db";
+import express from 'express';
+import {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} from './db';
 
 const app = express();
 
@@ -7,18 +12,18 @@ const port = 3500;
 
 app.get('/', (request, response) => {
   response.json({
-    info: 'Node.js, Express, and Postgres API'
-});
+    info: 'Node.js, Express, and Postgres API',
+  });
 });
 
 
 // API end points
 app.get('/api/v1/employees', getUsers);
-app.post('/api/v1//employees', createUser)
-app.put('/api/v1//employees/:id', updateUser)
-app.delete('/api/v1//employees/:id', deleteUser)
+app.post('/api/v1//employees', createUser);
+app.put('/api/v1//employees/:id', updateUser);
+app.delete('/api/v1//employees/:id', deleteUser);
 
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log(`App running on port ${port}`);
 });
