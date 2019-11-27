2afe53be446360cd6747c72a65decfe3b1bd28f3
manaraph <manaraph225@gmail.com>
2019-11-27 01:18:31 +0100

    Seperate routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import express from 'express';
-import { getUsers } from './db';
-import { signIn, createNewUser } from "./controllers/auth";
-import { createArticle, editArticle, deleteArticle } from "./controllers/articles";
-import { createComment } from "./controllers/articles/comments";
-import { requireAuth, requireSignIn, requireAdminAuth, } from "./services/middleware/auth";
+import routes from "./routes";
 
 const bodyParser = require('body-parser');
 
@@ -12,14 +8,7 @@ app.use(bodyParser.json());
 
 const port = 3500;
 
-// API end points
-app.get('/api/v1/employees', requireAdminAuth, getUsers);
-app.post('/api/v1/create-user', requireAdminAuth, createNewUser);
-app.post('/api/v1/signin', requireSignIn, signIn);
-app.post('/api/v1/articles', requireAuth, createArticle);
-app.put('/api/v1/articles/:articleId', requireAuth, editArticle);
-app.delete('/api/v1/articles/:articleId', requireAuth, deleteArticle);
-app.post('/api/v1/articles/:articleId/comments', requireAuth, createComment);
+app.use('/api/v1', routes );
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { getUsers } from '../db';
+import { signIn, createNewUser } from "../controllers/auth";
+import { createArticle, editArticle, deleteArticle } from "../controllers/articles";
+import { createComment } from "../controllers/articles/comments";
+import { requireAuth, requireSignIn, requireAdminAuth, } from "../services/middleware/auth";
+
+const routes = express.Router();
+
+// API end points
+routes.get('/employees', requireAdminAuth, getUsers);
+routes.post('/create-user', requireAdminAuth, createNewUser);
+routes.post('/signin', requireSignIn, signIn);
+routes.post('/articles', requireAuth, createArticle);
+routes.put('/articles/:articleId', requireAuth, editArticle);
+routes.delete('/articles/:articleId', requireAuth, deleteArticle);
+routes.post('/articles/:articleId/comments', requireAuth, createComment);
+
+module.exports = routes;
